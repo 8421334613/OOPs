50bf058ca8b37902b37126ba558a5928c156accb
sakhare1579 <bharatsakhare1579@gmail.com>
2025-09-18 12:44:51 +0530

    this structure of project

--- 1_opps.py
+++ 1_opps.py
@@ -1,14 +1,38 @@
-class employee():
+# class employee():
     
-    def __init__(self):
-        self.id=101
-        self.sallary=50000
-        self.designation="banglore"
+#     def __init__(self):
+#         self.id=101
+#         self.sallary=50000
+#         self.designation="banglore"
         
-    def travel(self,designation):
-        print(f"my desgnation is {designation} and its my dream city")
+#     def travel(self,designation):
+#         print(f"my desgnation is {designation} and its my dream city")
 
-sam=employee()
-print(sam.id)
-sam.travel("banglore")
+# sam=employee()
+# print(sam.id)
+# sam.travel("banglore")
     
+    
+class chatbot:
+    def __init__(self):
+        self.username="sam"
+        self.pasword=23
+        self.login=True
+        self.menu()
+        
+        
+    def menu(self):
+        num=int(input("1.login\n2.logout\n3.write a post\n4.massage a friend\n5.exit\n"))
+        
+        if num==1:
+            pass
+        elif num==2:
+            pass
+        elif num==3:
+            pass
+        elif num==4:
+            pass
+        elif num==5:
+            exit()
+        
+onj=chatbot()
